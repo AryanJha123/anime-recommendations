e9203139d335d21be7b5c9962486296609a39427
Aryan Jha <toaryanjha@gmail.com>
2026-07-24 19:28:47 -0400

    more show info

--- anime-recs/src/App.js
+++ anime-recs/src/App.js
@@ -35,19 +35,40 @@ function App() {
         <form onSubmit={handleSubmit} className="flex gap-10">
         <input className="text-black px-4 rounded-lg"/>
         <button className="bg-black px-4 py-2 rounded-xl" type="submit">Submit</button>
-        </form>
-        <ul>
-        {data.map((show) => (
-          <li key={show.id}>
-            <a className="h-auto mt-auto mb-auto" href={"https://anilist.co/anime/"+show.id} target="_blank" 
-            rel="noopener noreferrer">
-            <p>{show.name}</p>
-            <img src={show.pic}/>
-            </a>
-          </li>
-        ))}
-      </ul>
+        </form> 
       </header>
+      <div className="outer-div bg-black">
+        {data.map((show) => (
+          <div key={show.id} className={`px-4 relative flex lg:flex-row flex-col lg:gap-24 ${data.indexOf(show) % 2 == 0 ? 'bg-green-100' : 'bg-blue-100'}`}>
+            <a className="h-auto mt-auto mb-auto" href={"https://anilist.co/anime/"+show.id} target="_blank" rel="noopener noreferrer">
+                <div>
+                  <img src={show.pic} className='relative w-64 lg:mt-0 mx-auto mt-4'/>
+                  <div className='lg:absolute lg:mx-0 mx-auto bg-gray-600 z-10 w-64 h-24 bottom-0 flex py-4 px-4 overflow-hidden bg-opacity-70'>
+                    <p className="my-auto mx-auto text-white">{show.name}</p>
+                  </div>
+                </div>
+            </a>
+            <div className="relative flex lg:flex-row flex-col lg:gap-24 gap-8 pt-8 pb-8">
+            <div className='mx-auto lg:gap-24 flex flex-col text-black w-24'>
+              <p className="font-bold underline text-xl"> <br/></p>
+              <p className="text-2xl">{data.indexOf(show)+1}</p>
+            </div>
+            <div className='mx-auto lg:gap-24 gap-4 flex flex-col text-black w-24'>
+              <p className="font-bold underline text-xl">Genres</p>
+              <p className='text-xl'>asdf</p>
+            </div>
+            <div className='mx-auto lg:gap-24 gap-4 flex flex-col text-black w-24'>
+              <p className="font-bold underline text-xl">Tags</p>
+              <p className='text-xl'>asdf</p>
+            </div>
+            <div className='mx-auto lg:gap-24 gap-4 flex flex-col text-black w-36'>
+              <p className="font-lato font-bold underline text-xl">Most similar to:</p>
+              <p className='text-xl'>asdf</p>
+            </div>
+            </div>
+          </div>
+        ))}
+    </div>
     </div>
   );
 }
